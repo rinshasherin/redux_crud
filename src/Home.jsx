ef8d51de3f5dae97defd9311f5de3b0b57079e06
rinshasherin <rinshasherinck2002@gmail.com>
2024-10-22 00:00:16 +0530

    'redux\'

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-// import Add from './components/Add'
 import { Link } from 'react-router-dom'
 import { deleteStudent } from './redux/studentSlice'
 
--- src/redux/studentSlice.js
+++ src/redux/studentSlice.js
@@ -1,40 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { data } from "../components/data";
-
-// const studentSlice = createSlice({
-//     name: 'students',
-//     initialState: data,
-//     reducers: {
-//         addUser: (state, action) => {
-//             console.log(action)
-//             state.push(action.payload)
-//         },
-//         editUser: (state, action) => {
-//             const { id, name, dept, age } = action.payload
-//             const std = state.find(student => student.id == id)
-//             if (std) {
-//                 std.name = name
-//                 std.dept = dept
-//                 std.age = age
-//             }
-//             else {
-
-//             }
-//         },
-//         deleteUser: (state, action) => {
-//             const { id } = action.payload
-//             return state.filter(student => student.id !== id)
-//         }
-//     }
-// })
-
-
-// export default studentSlice.reducer
-// export const { addUser, editUser, deleteUser } = studentSlice.actions
-
-
-
-
 import { createSlice } from "@reduxjs/toolkit";
 import { data } from "../components/data"
 
@@ -53,7 +16,6 @@ const studentSlice = createSlice({
             const { id, name, dept, age } = action.payload
             const std = state.find(student => student.id == id)
             if (std) {
-                // std.id = id;
                 std.name = name;
                 std.dept = dept;
                 std.age = age
